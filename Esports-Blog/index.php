b7fc8e333d7ce31c7467d220e9bfb78caaac0b27
Livio Fran Mascarenhas <47179761+Livio30@users.noreply.github.com>
2020-11-20 23:54:11 +0530

    Add files via upload

--- Esports-Blog/index.php
+++ Esports-Blog/index.php
@@ -1,40 +1,55 @@
-<?php 
-    include("libs/header.php");
+<?php
+include("libs/header.php");
 
-    $query= "SELECT * FROM post";
-    $posts = $dataBase->query($query);
-?>  
-<!-- start blog preview cards -->
-  <?php if($posts->num_rows > 0){
-    while($row = $posts->fetch_assoc()){
-    ?>
-    <div class="card col-sm-4" style="width: 18rem; ">
-      <!-- background: url(./img/); to card -->
-      <div class="card-body">
-        <h5 class="card-title"><a href="singlePost.php?id=<?php echo $row['post_id'];?>"><?php echo $row['blog_title'];?></a></h5>
-        <h6 class="post-author">by <?php echo $row['author_name'];?></h6>
-        <p class="card-text">
-        <?php $content = $row['blog_content'];
-              echo substr($content , 0 , 150)."...";
-        ?>
-        </p>
-        <p class="card-text"><?php echo $row['blog_date'];?></p>
-        <div class="py-1 justify-content-center">
-          <a href="https://<?php echo $row['fb_link'];?>" target="_blank">
-            <i class="fa fa-facebook-square fa-lg pl-2" aria-hidden="true"></i
-          ></a>
-          <a href="https://<?php echo $row['insta_link'];?>" target="_blank"
-            ><i class="fa fa-instagram fa-lg pl-2" aria-hidden="true"></i
-          ></a>
-          <a href="https://<?php echo $row['twit_link'];?>" target="_blank"
-            ><i class="fa fa-twitter fa-lg pl-2" aria-hidden="true"></i
-          ></a>
-        </div>
-      </div>
-    </div>
-  <?php } }?>
-    <!-- end blog preview card  -->
-<?php 
-    include("libs/contact.html");
-    include("libs/footer.php");
-?>  
+$query = "SELECT * FROM post";
+$posts = $dataBase->query($query);
+?>
+
+<?php
+include('carousel.php');
+?>
+</div>
+
+<!-- End of  Header-->
+<!-- Start of Main -->
+<main>
+  <!-- start container div -->
+  <div class="container-fluid my-4">
+    <div class="row align-items-center justify-content-evenly">
+      <!-- start blog preview cards -->
+      <?php if ($posts->num_rows > 0) {
+        while ($row = $posts->fetch_assoc()) {
+      ?>
+          <div class="card col-sm-4" style="  width: 18rem;
+    height: 17.5rem;
+    margin-bottom: 10px;">
+            <!-- background: url(./img/); to card -->
+            <div class="card-body">
+              <h5 class="card-title"><a href="singlePost.php?id=<?php echo $row['post_id']; ?>">
+                  <?php $head = $row['blog_title'];
+                  echo substr($head, 0, 20) . " ...";
+                  ?>
+                </a></h5>
+              <h6 class="post-author">by <?php echo $row['author_name']; ?></h6>
+              <p class="card-text">
+                <?php $content = $row['blog_content'];
+                $ss = substr($content, 0, 90);
+                echo $ss . "...";
+                ?>
+              </p>
+              <a href="singlePost.php?id=<?php echo $row['post_id']; ?>"><button type="button" class="btn btn-primary btn-sm">Read more</button></a>
+              <p class="card-text"><?php echo $row['blog_date']; ?></p>
+              <div class="py-1 justify-content-center ">
+                <a class="social-icons" href="https://<?php echo $row['fb_link']; ?>" target="_blank"><img src="icons/fb.png" alt="Facebook"></a>
+                <a class="social-icons" href="https://<?php echo $row['insta_link']; ?>" target="_blank"><img src="icons/insta.png" alt="Instagram"></a>
+                <a class="social-icons" href="https://<?php echo $row['twit_link']; ?>" target="_blank"><img src="icons/twitter.png" alt="Twitter"></i></a>
+              </div>
+            </div>
+          </div>
+      <?php }
+      } ?>
+      <!-- end blog preview card  -->
+      <?php
+      include("libs/contact.html");
+      include("libs/footer.php");
+      ?>
