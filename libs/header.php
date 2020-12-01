b06bd2bcfee2d3925a842a8568c39e4378bf39b1
Livio30 <liviofranm@gmail.com>
2020-12-01 19:28:30 +0530

    Added Favicon

--- libs/header.php
+++ libs/header.php
@@ -17,7 +17,7 @@ include("libs/db.php");
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-lite.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-lite.min.js"></script>
-    <title>E-Sports Blog</title>
+    <title>E-Sports Blog </title>
     <link rel="apple-touch-icon" sizes="180x180" href="./siteImg/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="./siteImg/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="./siteImg/favicon-16x16.png">
